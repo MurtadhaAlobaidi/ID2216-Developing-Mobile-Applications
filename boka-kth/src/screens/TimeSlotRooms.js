ff7daa3db69758d08260bf27826e6f93ec7abc70
mhaao@kth.se <mhaao@kth.se>
2023-03-07 22:45:16 +0100

    TimeSlotRooms  update

--- boka-kth/src/screens/TimeSlotRooms.js
+++ boka-kth/src/screens/TimeSlotRooms.js
@@ -15,10 +15,8 @@ export default function TimeSlotRooms({ navigation, route }) {
     const { roomId, roomDocId } = route.params;//hämta from home 
     const [selectedTimeSlot, setSelectedTimeSlot] = React.useState('');
 
-
     console.log("Soso roomId : " + roomId);
     console.log("Soso doc.id : " + roomDocId);
-
 
     const [roomsNew, setRooms] = React.useState([]);
 
@@ -58,129 +56,35 @@ export default function TimeSlotRooms({ navigation, route }) {
     }
 
 
-
-
-
-
-
-
-
-
-
-    const onTimeSlotPress = (timeSlot) => {
-        setSelectedTimeSlot(timeSlot);
-
-        // Add your code to book the room here
-        console.log("Soso 1: " + timeSlot);
-
-        let book = async (room, start, end, itemId, timeSlotId, setTimeSlotRooms) => {
-            let bookedTimeSlots = [];
-            let bookingUser = [];
-
-            if (nrBookedHours < 2) {
-                const roomIndex = room - 301;
-                const timeSlotRoomsCopy = [...TimeSlotRooms];
-
-                if (timeSlotRoomsCopy[roomIndex].slots[timeSlotId].booked) {
-                    setErrorMessage("Room is already booked, please choose another room that is available");
-                } else {
-                    //creating a booking in the database
-                    const docRef = await addDoc(collection(db, "bookings"), {
-                        roomId: room,
-                        start: start,
-                        end: end,
-                        date: new Date().toLocaleString(),
-                        user: auth.currentUser.email,
-                        timeSlotId: timeSlotId
-                    });
-
-                    //updating the values of the room in the database
-                    const q = query(collection(db, "rooms"), where("roomId", "==", room));
-                    const querySnapshot = await getDocs(q);
-
-                    querySnapshot.forEach((doc) => {
-                        let item = doc.data()
-                        bookedTimeSlots = item.bookedTimeSlots;
-                        bookingUser = item.bookingUser;
-                    });
-                    bookedTimeSlots[timeSlotId] = true;
-                    bookingUser[timeSlotId] = auth.currentUser.email;
-                    const ref = doc(db, "rooms", itemId);
-                    setDoc(ref, { bookedTimeSlots: bookedTimeSlots }, { merge: true });
-                    setDoc(ref, { bookingUser: bookingUser }, { merge: true });
-
-                    //updating TimeSlotRooms state to reflect the booking
-                    timeSlotRoomsCopy[roomIndex].slots[timeSlotId] = {
-                        booked: true,
-                        user: auth.currentUser.email
-                    };
-                    setTimeSlotRooms(timeSlotRoomsCopy);
-
-                    /*
-                    //updateing the booker's bookedHours-field in the database
-                    const ref2 = doc(db, "Users", userId)
-                    setDoc(ref2, { bookedHours: (nrBookedHours + 1) }, { merge: true })
-                    console.log(nrBookedHours)
-                    setNrBookedHours(nrBookedHours + 1)
-                    setIsLoading(false);
-    
-                    refresh(500);*/
-                }
-            } else {
-                setErrorMessage("Maximum number of booked hours has been reached, Please cancel or change your current booking!");
-            };
-        };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*******/
-    };
-
-    const renderTimeSlot = (timeSlot) => {
-        return (
-            <TouchableOpacity
-                key={timeSlot}
-                onPress={() => onTimeSlotPress(timeSlot)}
-                style={selectedTimeSlot === timeSlot ? TimeSlotRoomsStyles.selectedTimeSlot : TimeSlotRoomsStyles.timeSlot}>
-                <Text>{timeSlot}</Text>
-            </TouchableOpacity>
-        );
-    };
-
-    const generateTimeSlots = () => {
-        const timeSlots = [];
-        for (let i = 8; i <= 18; i++) {
-            const timeSlot = `${i < 10 ? '0' : ''}${i}:00`;
-            timeSlots.push(timeSlot);
-        }
-        return timeSlots;
-    };
-
     const renderTimeSlots = () => {
-        const timeSlots = generateTimeSlots();
+        const timeSlots = [];
+        for (let i = 0; i < 10; i++) {
+            const timeSlotId = i;
+            const startTime = `${i + 8 < 10 ? '0' : ''}${i + 8}:00`;
+            const endTime = `${i + 9 < 10 ? '0' : ''}${i + 9}:00`;
+            const timeSlot = `${startTime} - ${endTime}`;
+            timeSlots.push({
+                id: timeSlotId,
+                timeSlot: timeSlot,
+                startTime: startTime,
+                endTime: endTime,
+            });
+        }
         return (
             <View style={TimeSlotRoomsStyles.timeSlotsContainer}>
-                {timeSlots.map(timeSlot => renderTimeSlot(timeSlot))}
+                {timeSlots.map(timeSlot => (
+                    <TouchableOpacity
+                        key={timeSlot.id}
+                        onPress={() => onTimeSlotPress(timeSlot)}
+                        style={selectedTimeSlot === timeSlot.id ? TimeSlotRoomsStyles.selectedTimeSlot : TimeSlotRoomsStyles.timeSlot}>
+                        <Text>{timeSlot.timeSlot}</Text>
+                    </TouchableOpacity>
+                ))}
             </View>
         );
     };
+
+
 
     let logout = () => {
         signOut(auth).then(() => {
